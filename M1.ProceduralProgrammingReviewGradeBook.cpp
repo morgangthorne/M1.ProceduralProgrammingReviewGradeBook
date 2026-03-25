8671ddf0d683fc89124b89f074016dacdf68e833
morgan thorne <morgangthorne@outlook.com>
2026-03-24 20:04:57 -0400

    Added the global const variable for AmountOfTestsPerStudent, also created multiple arrays in main to be passed through functions. Currently writing logic to read through txt file correctly.

--- M1.ProceduralProgrammingReviewGradeBook.cpp
+++ M1.ProceduralProgrammingReviewGradeBook.cpp
@@ -5,34 +5,45 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 
 using namespace std;
 
-int OpenFile();
+int OpenFile(string filename);
+void ReadThroughFile(string filename);
+
+//Global Const for number of tests
+const int AmountOfTestsPerStudent = 5;
 
 int main()
 {
-
-    OpenFile();
-
+    const int MaxStudents = 100;
     
+    string filename = "StudentGrades.txt";
+    string Names[MaxStudents];
+    
+    int Score[MaxStudents][AmountOfTestsPerStudent];
+    int NumberOfStudents = 0;
+    double Averages[MaxStudents];
+ 
+    OpenFile(filename);
+
+    ReadThroughFile(filename);
 }
 
-int OpenFile() {
-    string filename = "StudentGrades.txt";
-
+int OpenFile(string filename) {
     ifstream inFile;
-
     inFile.open(filename);
 
     if (inFile.fail()) {
         cout << "Error opening file.\n";
         return 1;
     }
+    inFile.close();
 }
 
-void ReadThroughFile() {
+void ReadThroughFile(string filename) {
     vector<string> names;
     vector<int> grades;
 
@@ -42,15 +53,16 @@ void ReadThroughFile() {
     int grade;
     
     ifstream inFile;
+    inFile.open(filename);
 
-    getline(inFile, line);
+    while (getline(inFile, line)) {
+        stringstream ss(line);
+        ss >> name;
 
-    names.push_back(name);
-    grades.push_back(grade);
-
-
-
-
-
+        for (int i = 0; i < AmountOfTestsPerStudent; i++) {
+            stringstream ss(line);
+            ss >> grade;
+        }
+    }
 
 }
